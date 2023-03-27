7843e5f9ce46071f5bfd592079beb7faf0ea14f3
svt13 <svt13@ukr.net>
2023-03-27 20:46:53 +0300

    add uow

---
+++ Chat.DataAccess/UnitOfWorks/UnitOfWorks.cs
@@ -0,0 +1,38 @@
+﻿using Chat.DataAccess.Repositories;
+using Chat.Domain.Interfaces;
+using Chat.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Chat.DataAccess.UnitOfWorks
+{
+    public class UnitOfWorks : IUnitOfWorks
+    {
+        private readonly ChatDbContext _db;
+        public IAbilityRepository Abilities { get; }
+        public IRequestRepository Requests { get; }
+        public IShoppingRepository Shoppings { get; }
+        public IUserRepository Users { get; }
+        public ISubscriptionRepository Subscriptions { get; }
+        public UserManager<UserModel> UserManager { get; }
+        public RoleManager<IdentityRole> Roles { get; }
+
+        public UnitOfWorks(ChatDbContext context, UserManager<UserModel> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = context;
+            Abilities = new AbilityRepository(context);
+            Requests = new RequestRepository(context);
+            Shoppings = new ShoppingRepository(context);
+            Users = new UserRepository(context);
+            Subscriptions = new SubscriptionRepository(context);
+
+            UserManager = userManager;
+            Roles = roleManager;
+        }
+
+        public async void Dispose()
+        {
+            await _db.SaveChangesAsync();
+            _db.Dispose();
+        }
+    }
+}
---
+++ Chat.Domain/Interfaces/IUnitOfWorks.cs
@@ -0,0 +1,11 @@
+﻿namespace Chat.Domain.Interfaces
+{
+    public interface IUnitOfWorks : IDisposable
+    {
+        IAbilityRepository Abilities { get; }
+        IRequestRepository Requests { get; }
+        IShoppingRepository Shoppings { get; }
+        IUserRepository Users { get; }
+        ISubscriptionRepository Subscriptions { get; }
+    }
+}
