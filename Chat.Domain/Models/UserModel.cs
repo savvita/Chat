daaa015f585da0455861a76e0de2c22bb2e2d26b
svt13 <svt13@ukr.net>
2023-03-27 23:00:42 +0300

    changed user model

--- Chat.Domain/Models/UserModel.cs
+++ Chat.Domain/Models/UserModel.cs
@@ -7,6 +7,8 @@ namespace Chat.Domain.Models
 
         public DateTime? BannedUntil { get; set; } = null;
         public int SubscriptionId { get; set; }
+        public DateTime? LastRequestDate { get; set; }
+        public int Requests { get; set; }
         public virtual SubscriptionModel? Subscription { get; set; }
         public virtual ICollection<ShoppingModel> ShoppingHistory { get; } = new List<ShoppingModel>();
         public virtual ICollection<RequestModel> RequestHistory { get; } = new List<RequestModel>();
